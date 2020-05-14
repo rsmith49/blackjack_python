3328d656eefde96f0cfee1a2eb2f632e5cc18573
Deniz Tumer <e.tumer.deniz@gmail.com>
2020-05-14 11:59:56 -0700

    Cleans up evaluation and training scripts
    This change cleans up the evaluation and training scripts

--- scripts/evaluate_tf_agent.py
+++ scripts/evaluate_tf_agent.py
@@ -10,6 +10,9 @@ def main():
     agent_type = 'dqn'
     agent_dir = f'data/{agent_type}'
     model_format = 'tensorflow'
+    start_updating = 5000
+    memory = 10000
+    num_episodes = 500000
 
     environment = TFBlackjackEnvironment(
         Deck(),
@@ -22,22 +25,18 @@ def main():
         directory=agent_dir,
         format=model_format,
         environment=environment,
-        start_updating=5000,
-        batch_size=1000,
-        memory=10000,
+        start_updating=start_updating,
+        memory=memory,
     )
 
-    for _ in range(500000):
-        # print()
-        # Initialize episode
+    for _ in range(num_episodes):
         states = environment.reset()
         terminal = False
 
         while not terminal:
             actions = agent.act(states=states, evaluation=True)
-            # print(f"Action Taken: {actions}")
+            _, terminal, _ = environment.execute(actions=actions)
             states, terminal, reward = environment.execute(actions=actions)
-            # agent.observe(terminal=terminal, reward=reward)
             environment.get_stats()
 
     environment.print_stats()
