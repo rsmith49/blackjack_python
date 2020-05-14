581fa91ccd7208c8302d802799b6e3f734cec30d
Deniz Tumer <e.tumer.deniz@gmail.com>
2020-05-14 11:59:56 -0700

    Cleans up evaluation and training scripts
    This change cleans up the evaluation and training scripts

--- scripts/train_tf_agent.py
+++ scripts/train_tf_agent.py
@@ -8,59 +8,72 @@ from src.simulator.player import Player, PassPlayerHandAgent
 
 def main():
     agent_type = 'dqn'
-    agent_dir = 'data/dqn'
+    agent_dir = f'data/{agent_type}'
     model_format = 'tensorflow'
-    tensorboard_dir = 'data/summaries'
+    tensorboard_dir = f'data/summaries/{agent_type}'
     tensorboard_labels = ['graph', 'entropy', 'kl-divergence', 'losses', 'rewards']
+    tensorboard_freq = 20
+    memory = 10000
+    batch_size = 20
+    num_episodes = 10000000
+    learning_rate = 3e-7
+    exploration = 0.2
+    summarizer = dict(
+        directory=tensorboard_dir,
+        labels=tensorboard_labels,
+        frequency=tensorboard_freq,
+    )
+    should_load = False
+    debug = False
 
     environment = TFBlackjackEnvironment(
         Deck(),
         SimpleDealer(),
-        Player(PassPlayerHandAgent(), ConstantBettingAgent())
+        Player(PassPlayerHandAgent(), ConstantBettingAgent()),
+        debug=debug
     )
 
-    # try to load agent that exists already, otherwise create a new agent instance
-    try:
+    if should_load:
         agent = Agent.load(
             directory=agent_dir,
             format=model_format,
             environment=environment,
-            start_updating=5000,
-            memory=10000,
-            summarizer=dict(
-                directory=tensorboard_dir,
-                labels=tensorboard_labels,
-                frequency=50,
-            )
+            memory=memory,
+            learning_rate=learning_rate,
+            exploration=exploration,
+            summarizer=summarizer
         )
         print("Loading existing agent for training")
-    except ValueError:
+    else:
         agent = Agent.create(
             agent=agent_type,
-            environment=environment,  # alternatively: states, actions, (max_episode_timesteps)
-            batch_size=10,
-            start_updating=5000,
-            memory=10000,
-            summarizer=dict(
-                directory=tensorboard_dir,
-                labels=tensorboard_labels,
-                frequency=50,
-            )
+            environment=environment,
+            batch_size=batch_size,
+            memory=memory,
+            exploration=exploration,
+            summarizer=summarizer
         )
         print("Creating new agent")
 
-    for ndx in range(100000):
-        # Initialize episode
+    # Train the agent on the number of episodes specified
+    for _ in range(num_episodes):
+        if debug:
+            print()
+
         states = environment.reset()
         terminal = False
 
         while not terminal:
             # Episode timestep
             actions = agent.act(states=states)
+
+            if debug:
+                print(f"ACTION TAKEN: {actions}")
+
             states, terminal, reward = environment.execute(actions=actions)
             agent.observe(terminal=terminal, reward=reward)
 
-    # save agent after running 300 episodes
+    # save agent after done training. Attaches num episodes trained on to agent name
     agent.save(directory=agent_dir, append='episodes')
 
     agent.close()
